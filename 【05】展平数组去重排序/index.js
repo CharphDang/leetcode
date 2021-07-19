5b4331680863a06eaacdfc7e8db28c01b64e3657
Charph <chaofengdang@gmail.com>
2021-07-19 17:37:10 +0800

    update

---
+++ 【05】展平数组去重排序/index.js
@@ -0,0 +1,24 @@
+const arr = [2, 3, 6, 1, [5, 6, 9, [1, 2], 3], 3, [1, 2], 10]
+
+// 展平数组 和 去重 -> 排序
+for (const [index, item] of arr.entries()) {
+  if (Array.isArray(item)) {
+    for (const key of item) {
+      arr.push(key)
+    }
+    arr.splice(index, 1)
+  }
+}
+
+const newArrSet = [...new Set(arr)]
+
+newArrSet.sort((pre, next) => {
+  return pre - next
+})
+
+console.log(newArrSet) // Charph-log
+
+// * es6 中 flat方法
+const arr2 = [2, 3, 6, 1, [5, 6, 9, [1, 2], 3], 3, [1, 2], 10]
+
+console.log(arr2.flat(Infinity)) // Charph-log
