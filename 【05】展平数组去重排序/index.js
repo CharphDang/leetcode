edfbd0f704548093f9ff1b6e8fd80dc60fffb296
Charph <chaofengdang@gmail.com>
2021-07-19 17:38:31 +0800

    update

--- 【05】展平数组去重排序/index.js
+++ 【05】展平数组去重排序/index.js
@@ -20,5 +20,5 @@ console.log(newArrSet) // Charph-log
 
 // * es6 中 flat方法
 const arr2 = [2, 3, 6, 1, [5, 6, 9, [1, 2], 3], 3, [1, 2], 10]
-
-console.log(arr2.flat(Infinity)) // Charph-log
+console.log(arr2.flat(1)) // *只展平一层 Charph-log
+console.log(arr2.flat(Infinity)) // *无论多少层都展开 Charph-log
